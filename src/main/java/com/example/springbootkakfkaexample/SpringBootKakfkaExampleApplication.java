ccc7a7e144a8ea59c4a2c36799e724423a33c8af
shreekhand.wankhede <shreekhand.wankhede@gmail.com>
2023-02-26 23:22:31 +0530

    Spring Boot kafka example initial commit

---
+++ src/main/java/com/example/springbootkakfkaexample/SpringBootKakfkaExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootkakfkaexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKakfkaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKakfkaExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootkakfkaexample/SpringBootKakfkaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootkakfkaexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKakfkaExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
